781286c0a465262e03587ca38b349a3bfeb34f3e
TCA166 <tomek.chady@gmail.com>
2023-04-14 22:46:04 +0200

    Made working commands for bot

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import nextcord
 from nextcord.ext import commands
-import datetime
 import json 
+import chartGen
+import matplotlib.pyplot as plt
+import io
+from datetime import datetime, timedelta
 
 #discord bot setup
 description = ''' '''
 intents = nextcord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='//', description=description, intents=intents)
-
-serverID = 691679955871989841
+bot = commands.Bot(command_prefix='$', description=description, intents=intents)
 
 async def handleChannel(channel:nextcord.channel) -> list:
     result = []
@@ -28,12 +29,12 @@ async def handleChannel(channel:nextcord.channel) -> list:
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('Attempting to fetch the server with id %d' %serverID)
+
+@bot.slash_command(description='Does a complete discord server scan, and sends back the json result.')
+async def initialise(interaction:nextcord.Interaction):
+    await interaction.send('Starting data gathering...')
     jsonR = {}
-    server = bot.get_guild(serverID)
-    if server == None:
-        print('Failed to get the server')
-        return
+    server = interaction.guild
     print('Fetched server with name:%s' % server.name)
     jsonR['name'] = server.name
     jsonR['desc'] = server.description
@@ -46,18 +47,83 @@ async def on_ready():
         memb['nick'] = member.nick
         jsonR['mbmr'].append(memb)
     jsonR['ownr'] = server.owner_id
-    jsonR['icon'] = server.icon.url
+    if server.icon is not None:
+        jsonR['icon'] = server.icon.url
     jsonR['emjs'] = [(e.name, e.url) for e in server.emojis]
     jsonR['chnl'] = {}
     for channel in server.channels:
         if not hasattr(channel, 'text_channels'): 
             jsonR['chnl'][channel.name] = await handleChannel(channel)
-            
+
     with open('%s.json' % server.name, 'w+') as f:
-        print(jsonR)
         f.write(json.dumps(jsonR))
+    await interaction.send(file=nextcord.File('%s.json' % server.name))
     print('Done')
-  
+
+@bot.slash_command(description='Creates a 3d message count graph. X=Time, Y=count, Z=channels.')
+async def message_count_chart(interaction:nextcord.Interaction, usr:nextcord.Member=None, ignore_channel:str='', contains:str=''):
+    if usr is not None:
+        usr = usr.id
+    ignore_channel = ignore_channel.split(',')
+    server = interaction.guild
+    try:
+        with open('%s.json' % server.name, 'r+') as f:
+            j = json.loads(f.read())
+    except:
+        interaction.send("Server hasn't been initialised. Cannot perform command.")
+        return
+    ax = chartGen.generateMsgCountChart(j, usr, ignore_channel, contains)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    file = nextcord.File(fp=buf, filename='plot.png')
+    await interaction.send(file=file)
+
+@bot.slash_command(description='Creates a pie chart of member activity.')
+async def active_usr_chart(interaction:nextcord.Interaction, ignore_channel:str='', max_date:str=None, min_date:str=None, contains:str=''):
+    if len(ignore_channel) == 0:
+        ignore_channel = ()
+    else:
+        ignore_channel = ignore_channel.split(',')
+    
+    if max_date is not None:
+        max_date = datetime.strptime(max_date[0:10], "%Y-%m-%d")
+    if min_date is not None:
+        min_date = datetime.strptime(min_date[0:10], "%Y-%m-%d")
+    server = interaction.guild
+    try:
+        with open('%s.json' % server.name, 'r+') as f:
+            j = json.loads(f.read())
+    except:
+        interaction.send("Server hasn't been initialised. Cannot perform command.")
+        return
+    ax = chartGen.generateActiveUsrChart(j, ignore_channel, max_date, min_date, contains)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    file = nextcord.File(fp=buf, filename='plot.png')
+    await interaction.send(file=file)
+
+@bot.slash_command(description='Creates a bar chart of word counts.')
+async def word_chart(interaction:nextcord.Interaction, ignore_channel:str='', ignore_urls:bool=True, cutoff:int=20, min_len:int=3, contains:str=''):
+    if len(ignore_channel) == 0:
+        ignore_channel = ()
+    else:
+        ignore_channel = ignore_channel.split(',')
+    server = interaction.guild
+    try:
+        with open('%s.json' % server.name, 'r+') as f:
+            j = json.loads(f.read())
+    except:
+        interaction.send("Server hasn't been initialised. Cannot perform command.")
+        return
+    ax = chartGen.generateWordChart(j, ignore_channel, True, ignore_urls, cutoff, min_len, contains)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    file = nextcord.File(fp=buf, filename='plot.png')
+    await interaction.send(file=file)
+
 with open("auth.json") as f:
     bot.run(json.loads(f.read())["key"])
 
--- chartGen.py
+++ chartGen.py
@@ -13,7 +13,7 @@ def handleMention(text:str, members:dict) -> str:
 def handleEmoji(text:str) -> str:
     return re.sub('<:.*:\d{18}>', re.findall('<(.*)\d{18}>', text)[0], text)
 
-def generateMsgCountChart(data:dict, usr:int=None, ignoreChannel:tuple=()) -> None:
+def generateMsgCountChart(data:dict, usr:int=None, ignoreChannel:tuple=(), contains:str='') -> plt.axes:
     ax = plt.figure().add_subplot(projection='3d')
 
     timeMaxs = []
@@ -32,7 +32,7 @@ def generateMsgCountChart(data:dict, usr:int=None, ignoreChannel:tuple=()) -> No
             times = [str(min(time) + timedelta(days=i))[0:10] for i in range(delta.days + 1)]
             values = {}
             for msg in channel:
-                if usr == None or msg[0] == usr:
+                if (usr == None or msg[0] == usr) and contains in msg[1]:
                     if msg[2][0:10] not in values.keys():
                         values[msg[2][0:10]] = 1
                     else:
@@ -50,8 +50,6 @@ def generateMsgCountChart(data:dict, usr:int=None, ignoreChannel:tuple=()) -> No
 
     ax.set_xlabel('Time', linespacing=3)
     ax.set_ylabel('Amount of messages')
-    if len(ignoreChannel) > 0:
-        ax.text(-2, 1.2, 'Ignored:%s' % str(ignoreChannel))
     if usr != None:
         ax.set_title('Messages by %s sent through time' % [u['name'] for u in data['mbmr'] if u['id'] == usr][0], pad=20)
     else:
@@ -64,16 +62,16 @@ def generateMsgCountChart(data:dict, usr:int=None, ignoreChannel:tuple=()) -> No
     ax.set_zticks([])
     years_fmt = dates.DateFormatter('%Y-%m')
     ax.xaxis.set_major_formatter(years_fmt)
-    plt.show()
+    return ax
 
-def generateActiveUsrChart(data:dict, ignoreChannel:tuple=(), maxDate:datetime=None, minDate:datetime=None) -> None:
+def generateActiveUsrChart(data:dict, ignoreChannel:tuple=(), maxDate:datetime=None, minDate:datetime=None, contains:str='') -> plt.axes:
     fig, ax = plt.subplots()
     counts = {}
     for key, channel in data['chnl'].items():
         if key not in ignoreChannel:
             for msg in channel:
                 date = datetime.strptime(msg[2][0:10], "%Y-%m-%d")
-                if (maxDate is None or date < maxDate) and (minDate is None or date > minDate):
+                if (maxDate is None or date < maxDate) and (minDate is None or date > minDate) and contains in msg[1]:
                     if msg[0] not in counts.keys():
                         counts[msg[0]] = 1
                     else:
@@ -84,15 +82,17 @@ def generateActiveUsrChart(data:dict, ignoreChannel:tuple=(), maxDate:datetime=N
         labels.append(usr)
     ax.pie(counts.values(), labels=labels, autopct='%1.1f%%')
     ax.set_title('Share of messages sent by user', pad=20)
+    if len(contains) > 0:
+        ax.text(-2, 1.3, 'Contains:%s' % contains)
     if len(ignoreChannel) > 0:
         ax.text(-2, 1.2, 'Ignored:%s' % str(ignoreChannel))
     if maxDate is not None:
         ax.text(-2, 1.1, 'Max date:%s' % str(maxDate)[0:10])
     if minDate is not None:
         ax.text(-2, 1, 'Min date:%s' % str(minDate)[0:10])
-    plt.show()
+    return ax
 
-def generateWordChart(data:dict, ignoreChannel:tuple=(), ignoreUTF:bool=True, ignoreUrls:bool=True, cutoff:int=20, minLen:int=3) -> None:
+def generateWordChart(data:dict, ignoreChannel:tuple=(), ignoreUTF:bool=True, ignoreUrls:bool=True, cutoff:int=20, minLen:int=3, contains:str='') -> plt.axes:
     fig, ax = plt.subplots()
     counts = {}
     for key, channel in data['chnl'].items():
@@ -106,7 +106,7 @@ def generateWordChart(data:dict, ignoreChannel:tuple=(), ignoreUTF:bool=True, ig
                         word = handleEmoji(word)
                     if '@' in word:
                         word = handleMention(word, data['mbmr'])
-                    if not (ignoreUrls and 'http' in word) and len(word) > minLen:
+                    if not (ignoreUrls and 'http' in word) and len(word) > minLen and contains in word:
                         if word not in counts.keys():
                             counts[word] = 1
                         else:
@@ -116,19 +116,21 @@ def generateWordChart(data:dict, ignoreChannel:tuple=(), ignoreUTF:bool=True, ig
     for word, count in counts.items():
         if count > cutoff:
             newCounts[word] = count
-    distance = 10
-    x = np.arange(len(newCounts.keys()) * distance, step=distance)
-    ax.bar(x, newCounts.values(), width=0.3 * distance)
+    x = np.arange(len(newCounts.keys()))
+    ax.bar(x, newCounts.values(), width=0.3)
     ax.set_xticks(x, newCounts.keys())
-    plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='center')
+    plt.setp(ax.get_xticklabels(), rotation=90, horizontalalignment='center')
     ax.set_title('Word frequency', pad=20)
     y = max(newCounts.values()) + 5
+    if len(contains) > 0:
+        ax.text(-2, 1.16, 'Contains:%s' % contains)
     if len(ignoreChannel) > 0:
         ax.text(-2, y + 0.12 * y, 'Ignored:%s' % str(ignoreChannel))
     if minLen > 0:
         ax.text(-2, y + 0.08 * y, 'Minimum length:%d' % minLen)
     ax.text(-2, y + 0.04 * y, 'Cutoff:%d occurences' % cutoff)
-    plt.show()
+    fig.tight_layout()
+    return ax
 
 if __name__ == "__main__":
     filename = input("Input json filename:")
@@ -136,5 +138,5 @@ if __name__ == "__main__":
         data = f.read()
     j = json.loads(data)
     #generateMsgCountChart(j)
-    #generateActiveUsrChart(j)
-    generateWordChart(j, ('testowanko'))
+    generateActiveUsrChart(j, contains='based')
+    #generateWordChart(j, ('testowanko'))
